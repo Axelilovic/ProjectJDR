e6f8ed2162fc117636069d6060d3778d2fd3d699
Patrice THORREZ <p.thorrez@ludus-academie.com>
2018-01-23 17:58:09 +0100

    Main menu screen: Buttons link to main_activity with intent (user type)

---
+++ app/src/main/java/jdr/projet/myapplication/Classes/Enumerations.java
@@ -0,0 +1,12 @@
+package jdr.projet.myapplication.Classes;
+
+/**
+ * Created by Zoidberg on 2018/01/23.
+ */
+
+public final class Enumerations {
+    public enum UserType{
+        PLAYER,
+        GAMEMASTER
+    } 
+}
--- app/src/main/java/jdr/projet/myapplication/Main_Menu_Activity.java
+++ app/src/main/java/jdr/projet/myapplication/Main_Menu_Activity.java
@@ -1,13 +1,44 @@
 package jdr.projet.myapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
-public class Main_Menu_Activity extends AppCompatActivity {
+import jdr.projet.myapplication.Classes.Enumerations;
+
+public class Main_Menu_Activity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+
+        // Check clicks on the view
+        findViewById(R.id.button_player_mode).setOnClickListener(this);
+        findViewById(R.id.button_gamemaster_mode).setOnClickListener(this);
     }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent;
+        switch(v.getId()){
+            case R.id.button_player_mode:
+                // We clicked on add comment
+                intent = new Intent(this, MainActivity.class);
+                intent.putExtra("PlayerType", Enumerations.UserType.PLAYER);
+                startActivity(intent);
+                break;
+            case R.id.button_gamemaster_mode:
+                // We clicked on add comment
+                intent = new Intent(this, MainActivity.class);
+                intent.putExtra("PlayerType", Enumerations.UserType.GAMEMASTER);
+                startActivity(intent);
+                break;
+            default:
+                break;
+        }
+
+    }
+
 }
